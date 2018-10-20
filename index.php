7a763b6a79b8988c3ed31417c946d1aefe6ffe86
judith <judithcynthia1@gmail.com>
2018-10-20 17:59:12 +0100

    First upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Who wants to be a Millionaire</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main_container">
+		<div class="jumbotron">
+			<div class="main_content">
+				<br><br><br><br><br>
+				<center>
+					<span id="bigText">Welcome to WWTBAM 2019</span>
+					<br>
+					<span id="smallText">Click on the Start button to Proceed</span>
+				</center>
+
+				<br><br>
+				<center>
+					<a href="/questions.php"><button class="button outline">
+						Get Started
+					</button></a>
+				</center>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
